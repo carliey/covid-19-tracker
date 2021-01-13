075e25ca6b4b3702f70c8d0c7c6a634c5e85599d
Khaleefa <mdcarliey@gmail.com>
2021-01-13 15:06:55 +0100

    Added leaflet map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { MapContainer, TileLayer } from 'react-leaflet'
 
 function Map() {
+    const position = [34.80746, -40.4796]
+    const zoom = 3;
+
     return (
-        <div>
-            The map
+        <div className="map">
+            <MapContainer center={position} zoom={zoom} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+               
+            </MapContainer>
         </div>
+        
     )
 }
 
